561f18a8ac8fd26c1a76430b937021b88aa4509e
Mats Olsberg <mats.olsberg@evry.com>
2019-04-11 13:58:09 +0200

    Add new component and load it

---
+++ src/components/BookList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div>
+            <h1>{{ title }}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'BookList',
+        data() {
+            return {
+                title: "All Books",
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    h1, h2 {
+        font-weight: normal;
+    }
+</style>
