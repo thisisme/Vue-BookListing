77a37f2303716e2c1780401292e5072e7a5273dc
Mats Olsberg <mats.olsberg@evry.com>
2019-04-11 14:32:17 +0200

    Add list of books

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,6 +2,9 @@
     <div>
         <div>
             <h1>{{ title }}</h1>
+            <ul>
+                <li v-for="book in books">{{ book.title}}: {{ book.author }}</li>
+            </ul>
         </div>
     </div>
 </template>
@@ -12,6 +15,11 @@
         data() {
             return {
                 title: "All Books",
+                books: [
+                    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                    {title: 'American Gods', author: 'Neil Gaiman'},
+                    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+                ],
             }
         }
     }
@@ -20,5 +28,13 @@
 <style scoped>
     h1, h2 {
         font-weight: normal;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: block;
+        margin: 0 10px;
     }
 </style>
